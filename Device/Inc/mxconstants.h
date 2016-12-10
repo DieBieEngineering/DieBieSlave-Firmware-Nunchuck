59aa82f55984b87703f5c1953d4b2d9c7711df3e
Danny Bokma <dannybokma@gmail.com>
2016-12-10 12:32:27 +0100

    Initial commit, just moved all files around, now fixxing project.

---
+++ Device/Inc/mxconstants.h
@@ -0,0 +1,81 @@
+/**
+  ******************************************************************************
+  * File Name          : mxconstants.h
+  * Description        : This file contains the common defines of the application
+  ******************************************************************************
+  *
+  * COPYRIGHT(c) 2016 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MXCONSTANT_H
+#define __MXCONSTANT_H
+  /* Includes ------------------------------------------------------------------*/
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private define ------------------------------------------------------------*/
+
+#define GPIO0_Pin GPIO_PIN_0
+#define GPIO0_GPIO_Port GPIOA
+#define GPIO1_Pin GPIO_PIN_1
+#define GPIO1_GPIO_Port GPIOA
+#define GPIO2_Pin GPIO_PIN_2
+#define GPIO2_GPIO_Port GPIOA
+#define LAN_SYNC0_Pin GPIO_PIN_4
+#define LAN_SYNC0_GPIO_Port GPIOC
+#define LAN_SYNC1_Pin GPIO_PIN_5
+#define LAN_SYNC1_GPIO_Port GPIOC
+#define SPI1_CS_Pin GPIO_PIN_12
+#define SPI1_CS_GPIO_Port GPIOB
+#define LAN_IRQ_Pin GPIO_PIN_13
+#define LAN_IRQ_GPIO_Port GPIOB
+#define LAN_SYNC0B14_Pin GPIO_PIN_14
+#define LAN_SYNC0B14_GPIO_Port GPIOB
+#define LED_DEBUG_Pin GPIO_PIN_15
+#define LED_DEBUG_GPIO_Port GPIOB
+#define I2C3_INT_Pin GPIO_PIN_8
+#define I2C3_INT_GPIO_Port GPIOC
+#define SPI3_CS_Pin GPIO_PIN_15
+#define SPI3_CS_GPIO_Port GPIOA
+#define I2C1_INT_Pin GPIO_PIN_5
+#define I2C1_INT_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+*/ 
+
+#endif /* __MXCONSTANT_H */
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ MDK-ARM/DieBieSlave/startup_stm32f303xe.d
@@ -0,0 +1,1 @@
+diebieslave\startup_stm32f303xe.o: startup_stm32f303xe.s
